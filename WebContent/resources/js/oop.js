0efcd13d17df99127ec8314a73b7322eee30f62a
taehyek0314 <taehyek0314@gamil.com>
2019-02-28 19:07:56 +0900

    2019-02-28 19:07

--- WebContent/resources/js/oop.js
+++ WebContent/resources/js/oop.js
@@ -5,16 +5,42 @@ function People(name,age,gender,job){
 	this.job = job;
 }
 
-function Cust(name,age,gender,job,grade,custNo){
-	People.call(this,name,age,gender,job);
+People.prototype.setName = name=>{this.name =name;};
+People.prototype.setAge = age=>{this.age = age;};
+People.prototype.setGender = gender=>{this.gender = gender;};
+People.prototype.setJob = job=>{this.job = job;};
+People.prototype.getName = ()=>{return this.name;};
+People.prototype.getAge = ()=>{return this.age;};
+People.prototype.getGender = ()=>{return this.gender;};
+People.prototype.getJob = ()=>{return this.job;};
+
+
+
+
+function Customer(name,age,gender,job,grade,custNo){
+	People.apply(this,arguments);
 	this.grade =grade;
 	this.custNo =custNo;
 }
-
+Customer.prototype = new People();
+Customer.prototype .setName = name=>{
+//	this.name = '[닉네임]'+name 이건 자바 문법
+	Object.getPrototypeOf(Customer.prototype).setName('[닉네임]'+name);
+	};
+Customer.prototype.setGrade = grade=>{this.grade =grade;};
+Customer.prototype.setCustNo = custNo=>{this.custNo = custNo;};
+Customer.prototype.getGrade = ()=>{return this.grade;};
+Customer.prototype.getCustNo = ()=>{return this.custNo;};
 var cust = (()=>{
 	let main =()=>{
-		let cus = new Cust('홍길동','25세','남','개발자','3급','123');
-		alert('고객정보 :'+cus.name+','+cus.age+','+cus.gender+','+cus.job+','+cus.grade+','+cus.custNo);
+		let cus = new Customer();
+		cus.setName('홍길동');
+		cus.setAge('25세');
+		cus.setGender('남');
+		cus.setJob('개발자');
+		cus.setGrade('3급');
+		cus.setCustNo('123');
+		alert('고객정보 :'+cus.getName()+','+cus.getAge()+','+cus.getGender()+','+cus.getJob()+','+cus.getGrade()+','+cus.getCustNo());
 	}
 	return {main : main};
 })();
@@ -62,53 +88,53 @@ var person =(()=>{
 })();
 
 
-var inherit =(()=>{
-	return{
-	main: ()=>{
-		let emp1 = new Employee();
-		emp1.setSalary("100");
-		emp1.setPosition("대리");
-		alert("직급,급여: "+emp1.getSalary()+','+emp1.getPosition());
-		
-		let cust1 = new Customer();
-		cust1.setCustNo('1234');
-		cust1.setGrade('실버등급');
-		alert("고객 번호, 등급 : "+cust1.getCustNo()+' , '+cust1.getGrade());
-		}
-	};
-})();
-
-function Employee(){
-	let _salary, _position;
-	return {
-		setSalary : (salary)=>{this._salary = salary;},
-		setPosition : (position)=>{this._position = position;},
-		getSalary : ()=>{return this._salary;},
-		getPosition : ()=>{return this._position;}
-	}
-}
-
-
-function Customer(){
-	let _custNo, _grade;
-	return {
-		setCustNo :(custNo)=>{this._custNo =custNo;},
-		setGrade :(grade)=>{this._grade =grade;},
-		getCustNo :()=>{return this._custNo;},
-		getGrade :()=>{return this._grade;}
-	}
-}
+//var inherit =(()=>{
+//	return{
+//	main: ()=>{
+//		let emp1 = new Employee();
+//		emp1.setSalary("100");
+//		emp1.setPosition("대리");
+//		alert("직급,급여: "+emp1.getSalary()+','+emp1.getPosition());
+//		
+//		let cust1 = new Customer();
+//		cust1.setCustNo('1234');
+//		cust1.setGrade('실버등급');
+//		alert("고객 번호, 등급 : "+cust1.getCustNo()+' , '+cust1.getGrade());
+//		}
+//	};
+//})();
+//
+//function Employee(){
+//	let _salary, _position;
+//	return {
+//		setSalary : (salary)=>{this._salary = salary;},
+//		setPosition : (position)=>{this._position = position;},
+//		getSalary : ()=>{return this._salary;},
+//		getPosition : ()=>{return this._position;}
+//	}
+//}
 
 
-var encap=(()=>{
-	var main =()=>{
-		person.setName('도날드 트럼프');
-		person.setAge('75');
-		person.setGender('남');
-		person.setJob('미국대통령');
-		alert('스펙: '+person.toString());
-	}
-	return {main : main};
-})();
+//function Customer(){
+//	let _custNo, _grade;
+//	return {
+//		setCustNo :(custNo)=>{this._custNo =custNo;},
+//		setGrade :(grade)=>{this._grade =grade;},
+//		getCustNo :()=>{return this._custNo;},
+//		getGrade :()=>{return this._grade;}
+//	}
+//}
 
+
+//var encap=(()=>{
+//	var main =()=>{
+//		person.setName('도날드 트럼프');
+//		person.setAge('75');
+//		person.setGender('남');
+//		person.setJob('미국대통령');
+//		alert('스펙: '+person.toString());
+//	}
+//	return {main : main};
+//})();
+//
 
